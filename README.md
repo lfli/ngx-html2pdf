60e2252a526871b5357716fc0719a473df8f44b0
lfli <haojierre@gmail.com>
2021-04-19 12:49:15 +0800

    fix: 高度超过 15000px 下载两次

--- README.md
+++ README.md
@@ -4,4 +4,10 @@ This library was generated with [Angular CLI](https://github.com/angular/angular
 
 ## 使用
 
-this.ngxHtml2pdfService.html2pdf(element-id: string, pdf-name: string, successCallback: Function, failCallback: Function)
+> AppModule 的 imports 中加入 NgxHtml2pdfModule
+
+> this.ngxHtml2pdfService.html2pdf(element-id: string, pdf-name: string, successCallback: Function, failCallback: Function)
+
+## 注意
+
+> 如果 element-id 元素的 height 超过 15000px，请将 element-id 元素的每个一级子元素占据一整行的宽度。
--- src/lib/ngx-html2pdf.service.ts
+++ src/lib/ngx-html2pdf.service.ts
@@ -42,39 +42,41 @@ export class NgxHtml2pdfService {
         }).catch(_ => {
           fail();
         })
-      }
+      } else {
 
-      const option = { allowTaint: true, useCORS: true };
-      html2canvas(ele, option).then((canvas) => {
+        const option = { allowTaint: true, useCORS: true };
+        html2canvas(ele, option).then((canvas) => {
 
-        let pdf: any = new jsPDF('p', 'mm', 'a4') //纵向，单位mm，A4纸张大小
-        let ctx: any = canvas.getContext('2d'); //预设2维画布
-        let a4w: number = 190;  //设置显示内容的大小，A4大小：210*297；最后显示在A4内部区域大小为：190*360
-        let a4h: number = 260;
-        let imgHeight: number = Math.floor(a4h * canvas.width / a4w); //按A4显示比例换算一页图像的像素高度
-        let renderedHeight: number = 0;
+          let pdf: any = new jsPDF('p', 'mm', 'a4') //纵向，单位mm，A4纸张大小
+          let ctx: any = canvas.getContext('2d'); //预设2维画布
+          let a4w: number = 190;  //设置显示内容的大小，A4大小：210*297；最后显示在A4内部区域大小为：190*260
+          let a4h: number = 260;
+          let imgHeight: number = Math.floor(a4h * canvas.width / a4w); //按A4显示比例换算一页图像的像素高度
+          let renderedHeight: number = 0;
 
-        while (renderedHeight < canvas.height) {//判断页面有内容时
-          let page: any = document.createElement('canvas'); //创建画布
-          page.width = canvas.width; //设置画布宽高等于内容宽高
-          page.height = Math.min(imgHeight, canvas.height - renderedHeight); //画布的高等于内容的最小的高度（不足一页）
-          //用getImageData裁剪指定区域，并绘制到前面创建的canvas对象中
-          let a: any = page.getContext('2d');
-          a.putImageData(ctx.getImageData(0, renderedHeight, canvas.width, Math.min(imgHeight, canvas.height - renderedHeight)), 0, 0);
-          pdf.addImage(page.toDataURL('image/jpeg', 1.0), 'JPEG', 10, 10, a4w, Math.min(a4h, a4w * page.height / page.width)); //添加图片到页面，保留10mm边距
+          while (renderedHeight < canvas.height) {//判断页面有内容时
+            let page: any = document.createElement('canvas'); //创建画布
+            page.width = canvas.width; //设置画布宽高等于内容宽高
+            page.height = Math.min(imgHeight, canvas.height - renderedHeight); //画布的高等于内容的最小的高度（不足一页）
+            //用getImageData裁剪指定区域，并绘制到前面创建的canvas对象中
+            let a: any = page.getContext('2d');
+            a.putImageData(ctx.getImageData(0, renderedHeight, canvas.width, Math.min(imgHeight, canvas.height - renderedHeight)), 0, 0);
+            pdf.addImage(page.toDataURL('image/jpeg', 1.0), 'JPEG', 10, 10, a4w, Math.min(a4h, a4w * page.height / page.width)); //添加图片到页面，保留10mm边距
 
-          renderedHeight += imgHeight;
-          if (renderedHeight < canvas.height) {
-            pdf.addPage();
+            renderedHeight += imgHeight;
+            if (renderedHeight < canvas.height) {
+              pdf.addPage();
+            }
           }
-        }
-        pdf.save(name + '.pdf')
-        tempDiv.remove();
-        success();
-      }).catch((res) => {
-        console.log(res);
-        fail();
-      });
+          pdf.save(name + '.pdf')
+          tempDiv.remove();
+          success();
+        }).catch((res) => {
+          console.log(res);
+          fail();
+        });
+
+      }
     }, 0);
   }
 
